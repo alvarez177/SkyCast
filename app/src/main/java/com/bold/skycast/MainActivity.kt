5528fa2ff2895bb83524c2501c8cd73047275c9b
alvarez177 <alvarez177.sebastian@gmail.com>
2026-02-01 09:51:33 -0500

    #2 - MVP splash screen

--- app/src/main/java/com/bold/skycast/MainActivity.kt
+++ app/src/main/java/com/bold/skycast/MainActivity.kt
@@ -5,12 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
 import com.bold.skycast.ui.theme.SkyCastTheme
@@ -19,40 +15,25 @@ import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val splashscreen = installSplashScreen()
-        var keepSplashScreen = true
         super.onCreate(savedInstanceState)
-        splashscreen.setKeepOnScreenCondition { keepSplashScreen }
-        lifecycleScope.launch {
-            delay(5000)
-            keepSplashScreen = false
-        }
+        setupSplashScreen()
         enableEdgeToEdge()
         setContent {
             SkyCastTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    innerPadding
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SkyCastTheme {
-        Greeting("Android")
+private fun ComponentActivity.setupSplashScreen() {
+    val splashscreen = installSplashScreen()
+    var keepSplashScreen = true
+    splashscreen.setKeepOnScreenCondition { keepSplashScreen }
+    lifecycleScope.launch {
+        delay(3000)
+        keepSplashScreen = false
     }
 }
