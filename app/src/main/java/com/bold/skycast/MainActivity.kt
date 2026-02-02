c48d53e5141691dc54e4cfbeb5a16f94da1d2759
alvarez177 <alvarez177.sebastian@gmail.com>
2026-02-01 19:16:33 -0500

    UI work #1 - First approach with static UI and its previews - Add ForecastItem.kt - Add ForecastSection.kt - Add LocationSearchBar.kt - Add CurrentWeatherStateSection.kt - Add SkyCastScreen.kt

--- app/src/main/java/com/bold/skycast/MainActivity.kt
+++ app/src/main/java/com/bold/skycast/MainActivity.kt
@@ -4,12 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
-import com.bold.skycast.ui.theme.SkyCastTheme
+import com.bold.skycast.presentation.SkyCastScreen
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -19,11 +16,7 @@ class MainActivity : ComponentActivity() {
         setupSplashScreen()
         enableEdgeToEdge()
         setContent {
-            SkyCastTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    innerPadding
-                }
-            }
+            SkyCastScreen()
         }
     }
 }
---
+++ app/src/main/java/com/bold/skycast/presentation/ForecastSection.kt
@@ -0,0 +1,49 @@
+package com.bold.skycast.presentation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ForecastSection() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = Color(0xFFF1EEEE),
+                shape = RoundedCornerShape(16.dp)
+            )
+            .padding(vertical = 10.dp)
+    ) {
+        Text(
+            modifier = Modifier.padding(10.dp),
+            text = "3 días de pronostico",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF263238)
+        )
+
+        ForecastItem(modifier = Modifier.padding(horizontal = 5.dp))
+        Spacer(modifier = Modifier.height(10.dp))
+        ForecastItem(modifier = Modifier.padding(horizontal = 5.dp))
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ForecastSectionPreview() {
+    ForecastSection()
+}
