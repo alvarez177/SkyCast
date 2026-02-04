eb9bd1c1c1b523e28fd51095551cd027186e24be
alvarez177 <alvarez177.sebastian@gmail.com>
2026-02-04 18:34:10 -0500

    I worked in the following: #4 - Add screen orientation

--- app/src/main/java/com/bold/skycast/presentation/ui/ScreenContent.kt
+++ app/src/main/java/com/bold/skycast/presentation/ui/ScreenContent.kt
@@ -1,14 +1,20 @@
 package com.bold.skycast.presentation.ui
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bold.skycast.presentation.model.CurrentWeatherVisualize
@@ -21,6 +27,21 @@ import com.bold.skycast.presentation.model.WeatherScreenInformationVisualize
 
 @Composable
 fun ScreenContent(
+    weatherScreenInformationVisualize: WeatherScreenInformationVisualize
+) {
+
+    val configuration = LocalConfiguration.current
+    val isLandscape = configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+
+    if (isLandscape) {
+        LandscapeContent(weatherScreenInformationVisualize)
+    } else {
+        PortraitContent(weatherScreenInformationVisualize)
+    }
+}
+
+@Composable
+fun PortraitContent(
     weatherScreenInformationVisualize: WeatherScreenInformationVisualize
 ) {
     Column(
@@ -43,6 +64,39 @@ fun ScreenContent(
         )
     }
 }
+
+@Composable
+fun LandscapeContent(
+    weather: WeatherScreenInformationVisualize
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+        ) {
+            CurrentWeatherStateSection(
+                location = weather.locationVisualize.name,
+                currentWeatherVisualize = weather.currentWeatherVisualize
+            )
+        }
+
+        Spacer(modifier = Modifier.width(12.dp))
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight()
+        ) {
+            ForecastSection(weather.forecastVisualize)
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
